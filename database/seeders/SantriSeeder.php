66458bdb9cf2e3a766dc69b04401ae663458070b
agiftsanyazhar <agiftsanyazhar@gmail.com>
2021-10-11 23:19:54 +0700

    ndak paham seeder

---
+++ database/seeders/SantriSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SantriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $santri = DB::table('santri')->get();
+
+        return view('dashboard.tables', ['santri' => $santri]);
+    }
+}
