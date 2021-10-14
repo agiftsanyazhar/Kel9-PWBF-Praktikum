e439efc8226bd47ef7599e5ac546c860b95f926a
agiftsanyazhar <agiftsanyazhar@gmail.com>
2021-10-14 18:24:35 +0700

    ok

--- resources/views/dashboard/charts.blade.php
+++ resources/views/dashboard/charts.blade.php
@@ -1,43 +1,43 @@
 @extends('layouts.dashboard')
 
 @section('container')
-<main>
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Charts</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
-            <li class="breadcrumb-item active">Charts</li>
-        </ol>
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-chart-area me-1"></i>
-                Area Chart
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Charts</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="dashboard">Dashboard</a></li>
+                <li class="breadcrumb-item active">Charts</li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-chart-area me-1"></i>
+                    Area Chart
+                </div>
+                <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>
+                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
             </div>
-            <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>
-            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <i class="fas fa-chart-bar me-1"></i>
-                        Bar Chart
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-bar me-1"></i>
+                            Bar Chart
+                        </div>
+                        <div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>
+                        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
                     </div>
-                    <div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>
-                    <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-pie me-1"></i>
+                            Pie Chart
+                        </div>
+                        <div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>
+                        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <div class="card mb-4">
-                    <div class="card-header">
-                        <i class="fas fa-chart-pie me-1"></i>
-                        Pie Chart
-                    </div>
-                    <div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>
-                    <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-                </div>
-            </div>
         </div>
-    </div>
-</main>
+    </main>
 @endsection
