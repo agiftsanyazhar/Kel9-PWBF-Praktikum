8b6b1bc29a082560543dcea6eefa0c59e1cdb12f
agiftsanyazhar <agiftsanyazhar@gmail.com>
2021-10-14 18:42:24 +0700

    setoran trakhir

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -732,15 +732,6 @@
       <div class="copyright">
         &copy; Copyright <strong><span>TPQ Al-Hikmah</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
-        <a href="#">Privacy Policy</a>
-        &middot;
-        <a href="#">Terms &amp; Conditions</a>
-      </div>
     </div>
   </footer><!-- End Footer -->
 
